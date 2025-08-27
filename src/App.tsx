792b99db2a5e2f57822edaac6bc23e36c5fa2557
daizzt10@gmail.com <daizzt10@gmail.com>
2025-08-28 00:10:16 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 // import Increase from "./components/bt1"
-import UserList from "./components/bt2"
+// import UserList from "./components/bt2"
+// import LoginForm from "./components/bt3"
+// import ColorPicker from "./components/bt4"
+// import InputText from "./components/bt5"
+// import InputRadio from "./components/bt6"
+import ToDoList from "./components/bt7"
+
 function App() {
 
 
   return (
     <>
       {/* <Increase></Increase> */}
-      <UserList></UserList>
+      {/* <UserList></UserList>
+      <LoginForm></LoginForm>
+      <ColorPicker></ColorPicker>
+      <InputText></InputText>
+      <InputRadio></InputRadio> */}
+      <ToDoList></ToDoList>
     </>
   )
 }
